b00a657c74baf9cc30274c7e5d3697e7d5473458
rmcdanielslc <rmcdnaielslc@gmail.com>
2017-03-03 15:52:33 -0700

    last problem

--- practice.js
+++ practice.js
@@ -6,20 +6,22 @@
 
 // Rewrite the following line so that inner is scoped to the if statement.
 if ( true ) {
-	var inner = "Inner!";
+	let inner = "Inner!";
 }
 
 // declare a variable named noChange that cannot be re-assigned and give it a value of 100.
-
+const noChange = 100;
 
 // declare a new constant named vocab with the value of [ "let", "var", "destructuring", "spread", "rest" ]
-
+const vocab = [ "let", "var", "destructuring", "spread", "rest" ]
 
 // change "var" to "const" at index 1 of vocab
 
+vocab[1] = 'const';
 
 // add "arrow function" to the end of vocab
 
+vocab.push('arrow function');
 
 
 ///////////////////////
@@ -28,11 +30,12 @@ if ( true ) {
 
 // Declare 2 new variables, first and last, and assign them your first and last name.
 
-
+var first = 'Ryan';
+var last = 'McDaniel';
 
 // Declare the variable fullName and set it equal to your full name using template literals
 
-
+var fullName = `${first} ${last}`;
 
 //////////////////////
 /* OBJECT LITERALS */
@@ -43,11 +46,20 @@ if ( true ) {
 // and lovesDevMountain should be set to true.
 // learnES6 should be a method created with object method shorthand that returns "This is awesome!"
 
-
+var me = {
+	first,
+	last,
+	learnES6: () => "This is awesome!",
+	lovesDevMountain: true
+}
 
 // using computed property names create a new object named flipped where the keys are your first and last name
 // and the values are "first" and "last"
 
+var flipped = {
+	[first]: 'first',
+	[last]: 'last'
+}
 
 //////////////////////////////
 /* ASSIGNMENT DESTRUCTURING */
@@ -63,8 +75,7 @@ const destructuring = {
 // using assignment destructuring create two new variables, objects and arrays,
 // with the values taken from the above object
 
-
-
+const {objects, arrays} = destructuring;
 
 // do not modify
 const vocabAgain = [ "let", "const", "destructuring", "spread", "rest", "arrow function" ];
@@ -72,6 +83,7 @@ const vocabAgain = [ "let", "const", "destructuring", "spread", "rest", "arrow f
 // using assignment destructuring, create three variables notVar, alsoNotVar, and everythingElse
 // notVar should be "let", alsoNotVar should be "const", and everythingElse should contain the rest of the array.
 
+const [notVar, alsoNotVar, ...everythingElse] = vocabAgain;
 
 
 ////////////
@@ -85,6 +97,7 @@ const lastFive = [ 6, 7, 8, 9, 10 ];
 // using the spread operator, create a new array named oneThroughTen.
 // You should use the above arrays (and 2 missing digits of your own)
 
+var oneThroughTen = [...firstThree,4,5,...lastFive]
 
 // do not modify
 const jsVersions = [ "es5", "es6", "es2017 onward" ];
@@ -99,14 +112,14 @@ function whatToUse( past, present, future ) {
 // using the spread operator, pass each element of jsVersions as an individual argument to whatToUse
 // and save the return value in a variable named currentJS.
 
-
+const currentJS = whatToUse(...jsVersions);
 
 ///////////////
 /* FUNCTIONS */
 ///////////////
 
 // modify greeter to greet Anonymous if no name is passed.
-function greeter( name ) {
+function greeter( name = 'Anonymous' ) {
 	return `Hi ${ name }!`
 }
 
@@ -115,12 +128,26 @@ function greeter( name ) {
 // and returns number to the power of exponent.
 // if no exponent is passed, the exponent should be two
 
+const toPower = (num, exp = 2) => num ** exp
+
 
 // using rest parameters, write a function named evenOdd that takes in
 // any number of parameters and returns an object with two properties - even and odd.
 // this function should check each parameter and push it to the appropriate property.
 
+function evenOdd(...params){
+	let newObj = {
+	};
 
+	params.forEach(number => {
+		if(number % 2 === 0){
+			newObj.even.push(number)
+		} else {
+			newObj.odd.push(numbers)
+		}
+	})
+	return newObj;
+}
 
 // write a function named multiply that takes in a num parameter and an arbitrary amount of
 // additional numbers. This function should return an array of each additional number
